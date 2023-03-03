49b3228a8e9e9de9c9967bedc628d611fcbe3d1e
Eloagbawe <eloagbawe@gmail.com>
2023-03-03 21:30:44 +0100

    add task 0 files

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This file contains the filter_datum function"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction:str, message: str,  seperator: str) -> str:
+    pass
