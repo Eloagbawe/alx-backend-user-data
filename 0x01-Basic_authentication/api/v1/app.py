be134c61191f9de88a11abe2cc904d4054941c2a
Eloagbawe <eloagbawe@gmail.com>
2023-03-07 00:52:56 +0100

    add solution to task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ if authType == 'auth':
     from api.v1.auth.auth import Auth
 
     auth = Auth()
+elif authType == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This file contains the BasicAuth class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """The Basic Auth class"""
