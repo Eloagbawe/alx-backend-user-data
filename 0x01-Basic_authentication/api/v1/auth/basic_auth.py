f33cda76394b61b49ceb457344151701adbd5da9
Eloagbawe <eloagbawe@gmail.com>
2023-03-07 02:23:41 +0100

    add solution to task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,3 +69,24 @@ class BasicAuth(Auth):
             return userList[0]
         else:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This method overloads Auth and retrieves
+        the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+        decoded_base64_header = self.decode_base64_authorization_header(
+                                    base64_header)
+        if decoded_base64_header is None:
+            return None
+        user_credentials = self.extract_user_credentials(decoded_base64_header)
+        if user_credentials is None:
+            return None
+        user = self.user_object_from_credentials(user_credentials[0],
+                                                 user_credentials[1])
+        return user
