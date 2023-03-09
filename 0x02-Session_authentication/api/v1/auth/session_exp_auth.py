1bc25adabfdb500835074928c34931b4477fd849
Eloagbawe <eloagbawe@gmail.com>
2023-03-09 23:23:44 +0100

    update session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -31,18 +31,32 @@ class SessionExpAut(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """user_id_for_session_id"""
+        # if session_id is None:
+        #     return None
+        # if session_id not in self.user_id_by_session_id.keys():
+        #     return None
+        # session_dict = self.user_id_by_session_id[session_id]
+        # if self.session_duration <= 0:
+        #     return session_dict.get('user_id')
+        # if session_dict.get('created_at') is None:
+        #     return None
+        # created_at = session_dict.get('created_at')
+        # duration = created_at + timedelta(seconds=self.session_duration)
+        #
+        # if duration < datetime.now():
+        #     return None
+        # return session_dict.get('user_id')
         if session_id is None:
             return None
-        if session_id not in self.user_id_by_session_id.keys():
+        user_details = self.user_id_by_session_id.get(session_id)
+        if user_details is None:
             return None
-        session_dict = self.user_id_by_session_id[session_id]
+        if "created_at" not in user_details.keys():
+            return None
         if self.session_duration <= 0:
-            return session_dict.get('user_id')
-        if session_dict.get('created_at') is None:
+            return user_details.get("user_id")
+        created_at = user_details.get("created_at")
+        allowed_window = created_at + timedelta(seconds=self.session_duration)
+        if allowed_window < datetime.now():
             return None
-        created_at = session_dict.get('created_at')
-        duration = created_at + timedelta(seconds=self.session_duration)
-
-        if duration < datetime.now():
-            return None
-        return session_dict.get('user_id')
+        return user_details.get("user_id")
