a82fba30828400eaae22f305bd52deb43b3969eb
Eloagbawe <eloagbawe@gmail.com>
2023-03-14 20:51:27 +0100

    update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,14 +47,13 @@ def login() -> Response:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> Response:
+def logout() -> None:
     """DELETE /sessions"""
     session_id = request.cookies.get('session_id')
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-            redirect('/', code=302)
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id and user:
+        AUTH.destroy_session(user.id)
+        redirect('/', code=302)
     abort(403)
 
 
