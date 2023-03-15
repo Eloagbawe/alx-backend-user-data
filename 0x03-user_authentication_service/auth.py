2d2e4e2469f42a452bc75b9722d082d2a11faa15
Eloagbawe <eloagbawe@gmail.com>
2023-03-15 13:47:45 +0100

    update app.py and auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,5 +97,6 @@ class Auth:
             hashed_password = _hash_password(password).decode('utf-8')
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
+            return None
         except NoResultFound:
             raise ValueError
