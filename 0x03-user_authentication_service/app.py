ac33c77470fae80b5f009555eaf531e09fc3a08a
Eloagbawe <eloagbawe@gmail.com>
2023-03-15 14:38:35 +0100

    update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,31 +30,32 @@ def add_user() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login() -> Response:
-    """POST /sessions"""
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """Handles login sessions"""
     email = request.form.get('email')
     password = request.form.get('password')
-    valid_login = AUTH.valid_login(email, password)
 
-    if valid_login:
+    valid_user_login = AUTH.valid_login(email, password)
+
+    if valid_user_login:
         session_id = AUTH.create_session(email)
-        res = jsonify({"email": "{}".format(email), "message": "logged in"})
-        res.set_cookie('session_id', session_id)
-        return res
-    else:
-        abort(401)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+
+    abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> Response:
-    """DELETE /sessions"""
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """The logout method"""
     session_id = request.cookies.get('session_id')
-    if session_id:
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-            return redirect('/')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('home'))
+
     abort(403)
 
 
