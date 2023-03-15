69934289e7fa3ce82d9cd7f8afad59c9d87d4745
Eloagbawe <eloagbawe@gmail.com>
2023-03-15 01:55:24 +0100

    update app.py and auth.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,30 +80,16 @@ def get_reset_password_token() -> Response:
         abort(403)
 
 
-# @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-# def update_password() -> Response:
-#     """PUT /update_password"""
-#     email = request.form.get('email')
-#     reset_token = request.form.get('reset_token')
-#     new_password = request.form.get('new_password')
-#
-#     try:
-#         AUTH.update_password(reset_token, new_password)
-#         return jsonify({"email": "{}".format(email),
-#                         "message": "Password updated"}), 200
-#     except ValueError:
-        abort(403)
-
-
-@app.route('/reset_password', methods=['PUT'])
-def update_password() -> str:
-    """ Updates a users password given a reset token """
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> Response:
+    """PUT /update_password"""
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
+
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": f"{email}",
+        return jsonify({"email": "{}".format(email),
                         "message": "Password updated"}), 200
     except ValueError:
         abort(403)
